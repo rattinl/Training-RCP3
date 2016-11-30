b7f67a986d82cdbfdbcd4836e45d6374f51db3cc
Ludovic RATTIN <ludovic.rattin@st.com>
2016-11-30 14:02:01 +0100

    Ajout de Node generic.
    Change-Id: Ia16a3eecc3d9fb532762cef5f1e1c346bd295b53
    Signed-off-by: Ludovic RATTIN <ludovic.rattin@st.com>

--- com.st.rental.ui/src/com/st/rental/ui/views/RentalProvider.java
+++ com.st.rental.ui/src/com/st/rental/ui/views/RentalProvider.java
@@ -7,6 +7,7 @@ import org.eclipse.jface.viewers.LabelProvider;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
 
 public class RentalProvider extends LabelProvider implements ITreeContentProvider {
 
@@ -23,8 +24,17 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		// TODO Auto-generated method stub
-		if ( parentElement instanceof RentalAgency )
-			return ((RentalAgency) parentElement).getCustomers().toArray() ;
+		if ( parentElement instanceof RentalAgency ) {
+			RentalAgency a = (RentalAgency) parentElement;
+			return new Node[] {  new Node (Node.CUSTOMERS, a),
+							new Node (Node.LOCATIONS, a),
+							new Node (Node.OBJETS_À_LOUER, a) };
+		}
+		if ( parentElement instanceof Node ) {
+			return ((Node) parentElement).getChildren();
+		}
+		//			return ((RentalAgency) parentElement).getCustomers().toArray() ;
+		
 		return null;
 	}
 
@@ -48,7 +58,40 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		else
 			if ( element instanceof Customer )
 				return ((Customer) element).getDisplayName();
+		if ( element instanceof RentalObject )
+			return ((RentalObject) element).getName();
 		return super.getText(element);
+	}
+	
+	
+	public class Node {
+		public static final String OBJETS_À_LOUER = "Objets à louer";
+		public static final String LOCATIONS = "Locations";
+		public static final String CUSTOMERS = "Customers";
+		private String name;
+		private RentalAgency agency;
+		
+		public Node(String name, RentalAgency agency) {
+			super();
+			this.name = name;
+			this.agency = agency;
+		}
+		
+		public Object[] getChildren(  ) {
+			if ( name == CUSTOMERS )
+				return ((RentalAgency) agency).getCustomers().toArray();
+			if ( name == LOCATIONS )
+				return ((RentalAgency) agency).getRentals().toArray();
+			if ( name == OBJETS_À_LOUER )
+				return ((RentalAgency) agency).getObjectsToRent().toArray();
+			
+			return null;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
 	}
 
 }
