ce0dc3cf81c4fdac106c848c934eea3986233225
Ludovic RATTIN <ludovic.rattin@st.com>
2016-12-01 18:21:07 +0100

    Ajout de commande conditionnelles sur Object. RCP70-2
    Change-Id: Icaaed221f97482acdda38eddb83fae55e74c04c3
    Signed-off-by: Ludovic RATTIN <ludovic.rattin@st.com>

---
+++ com.st.rental.ui.cmd/src/com/st/rental/ui/cmd/handlers/CopyCustomerHandlers.java
@@ -0,0 +1,56 @@
+package com.st.rental.ui.cmd.handlers;
+
+import java.util.Iterator;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.ImageTransfer;
+import org.eclipse.swt.dnd.RTFTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.opcoach.training.rental.Customer;
+import com.st.rental.ui.RentalUIActivator;
+import com.st.rental.ui.RentalUIConstants;
+
+public class CopyCustomerHandlers extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		String textData = "Hello World";
+		
+		Clipboard clipboard = new Clipboard(Display.getCurrent());
+		ISelection currentSelection = HandlerUtil.getCurrentStructuredSelection(event);
+
+		if ( currentSelection instanceof IStructuredSelection ) {
+			IStructuredSelection isel = ( IStructuredSelection ) currentSelection;
+			Object selected = isel.getFirstElement();
+			if ( selected instanceof Customer ) {
+				textData = ((Customer) selected).getDisplayName();
+				String rtfData = "{\\rtf1\\b\\i " + textData + "}";
+
+				TextTransfer textTransfer = TextTransfer.getInstance();
+				RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+				Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer,ImageTransfer.getInstance()};
+				Object[] data = new Object[]{textData, rtfData,RentalUIActivator.getDefault().getImageRegistry().get(RentalUIConstants.IMG_AGENCY).getImageData()};
+
+				clipboard.setContents(data, transfers);
+				clipboard.dispose();
+
+			}
+				
+		}
+		
+		
+		return null;
+	}
+
+	
+}
